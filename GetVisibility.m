f845fd38b603384182541bc42abb3f2cf55ade5e
ejesper <jesperericsson@outlook.com>
2014-11-19 22:04:46 +0100

    visibility matrix done, not included in updates

---
+++ GetVisibility.m
@@ -0,0 +1,20 @@
+function visibilityMatrix = GetVisibility(boidPositions, visibilityRange)
+%getVisibility Returns a n x n matrix with which visible for all boids.
+%With the visible neigbours as 1 in the row vector for each boid. 
+
+  [numberOfBoids, dimensions] = size(boidPositions);
+  visibilityMatrix = zeros(numberOfBoids,numberOfBoids);
+  for i = 1:numberOfBoids
+    for j = i+1:numberOfBoids
+
+      distance = sqrt(sum((boidPositions(i,:) - boidPositions(j,:)).^2));
+
+      if(distance < visibilityRange)
+        visibilityMatrix(i,j) = 1;
+        visibilityMatrix(j,i) = 1;
+      end
+
+    end
+  end
+end
+
--- Main.m
+++ Main.m
@@ -2,7 +2,7 @@
 %clear all; close all; clc;
 
 numberOfIterations = 2000;
-numberOfBoids = 100;
+numberOfBoids = 5;
 deltaT = 0.5;
 
 % Parameters
@@ -13,11 +13,13 @@ separationRadius = 20;
 maxVelocity = 3;
 maxPositions = [100,100,100];
 restrictionFactor = 0.1;
+visibilityRange = 30;
 
 % Putting all parameters in a vector to reduce nr of parameters into
 % functions. Remember that order is important here!
 paramVector = [cohesionFactor, alignmentFactor, separationFactor, ...
-    separationRadius, maxVelocity, maxPositions,restrictionFactor];
+    separationRadius, maxVelocity, maxPositions,restrictionFactor, ...
+    visibilityRange];
 
 % Initialise simulation
 boidPositions = InitializePositions(numberOfBoids, maxPositions);
@@ -26,14 +28,16 @@ boidVelocities = zeros(numberOfBoids,3);
 [posHandler,velHandler] = PlotBoids(boidPositions,boidVelocities,maxPositions);
 %%
 for i = 1:numberOfIterations
+  
+  %working function but not included in updates
+  visibilityMatrix = GetVisibility(boidPositions, visibilityRange);
   for iBoid = 1:numberOfBoids
     
     boidVelocities(iBoid,:) = UpdateBoidVelocity(boidPositions, ...
       boidVelocities, iBoid, paramVector);
     
   end
-  
-  
+    
   boidPositions = boidPositions + deltaT.*boidVelocities;
   
   %pause(0.01)
